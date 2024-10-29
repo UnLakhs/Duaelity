17e428d7d2733714a36d42e79b4cc4095046daa7
UnLakhs <apostolos052@gmail.com>
2024-10-29 12:30:42 +0200

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { IoIosSearch } from "react-icons/io";
+import { FaTrophy } from "react-icons/fa";
+
+//to avoid repetition of styles, we can create a variable for the styles
+const navDivStyles = `flex gap-2 cursor-pointer hover:bg-white hover:text-black transition duration-300 p-4 justify-start items-center`;
+
+const NavBar = () => {
+  return (
+    <nav className="bg-[#18283B] text-[#8392A5] fixed h-full w-36">
+      <div className="flex justify-center">
+        <img
+          src="images/Duaelity_logo.png"
+          alt="Duaelity logo"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className={`${navDivStyles}`}> 
+        <FaTrophy size={20}/>
+        <span>Tournaments</span>
+      </div>    
+      <div className={`${navDivStyles}`}>
+        <IoIosSearch />
+        <span>Search</span>
+      </div> 
+      <div className={`${navDivStyles}`}>
+        <IoIosSearch />
+        <span>Search</span>
+      </div> 
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import NavBar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="ml-36">
+        This is a mf
+      </div>
+    </>
+  );
+}
