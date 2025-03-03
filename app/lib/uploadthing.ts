e13dd4d9d37716b24c4fda8472ca635396faa656
UnLakhs <apostolos052@gmail.com>
2025-03-03 17:02:52 +0200

    removed previous uploadthing

--- app/lib/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-// import { createUploadthing } from "uploadthing/next";
-// const ourFileRouter = createUploadthing({
-//     fileTypes: ["image"],
-//     maxSize: "4MB",
-//   });
-  
-//   export const { useUpload } = ourFileRouter;
