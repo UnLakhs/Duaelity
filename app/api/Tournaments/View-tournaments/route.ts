3ba4d51e7212537fd7669ae2dedb7898756497b2
UnLakhs <apostolos052@gmail.com>
2025-03-05 01:30:46 +0200

    search and pagination route

--- app/api/Tournaments/View-tournaments/route.ts
+++ app/api/Tournaments/View-tournaments/route.ts
@@ -1,12 +1,28 @@
+import { Tournament } from "@/app/Cosntants/constants";
 import clientPromise from "@/app/lib/mongoDB";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  //Search
+  const { searchParams } = new URL(req.url);
+  const search = searchParams.get("search") || ""; // search term
+
+  //pagination
+  const page = parseInt(searchParams.get("page") || "1");
+  const limit = parseInt(searchParams.get("limit") || "10");
+  const skip = (page - 1) * limit;
+
+  const query = search ? { name: { $regex: search, $options: "i" } } : {};
+
   try {
     const client = await clientPromise;
     const db = client.db("Duaelity");
     const tournaments = db.collection("tournaments");
-    const allTournaments = await tournaments.find().toArray();
+    const allTournaments = await tournaments
+      .find<Tournament>(query)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
 
     return NextResponse.json(allTournaments);
   } catch (error) {
