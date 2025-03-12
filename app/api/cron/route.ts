c2bfd26ef0ba2c2a44844c221c0e5d27957fa2e6
UnLakhs <apostolos052@gmail.com>
2025-03-12 12:44:47 +0200

    added cron job to update a tournament status based on the date

---
+++ app/api/cron/route.ts
@@ -0,0 +1,48 @@
+import clientPromise from "@/app/lib/mongoDB";
+import { NextResponse } from "next/server";
+
+//Helper function to calculate the state of a tournament
+const calculateTournamentSate = (startDate: Date, endDate: Date) => {
+  const currentDate = new Date();
+  if (startDate > currentDate) {
+    return "upcoming";
+  } else if (startDate >= currentDate && endDate <= currentDate) {
+    return "ongoing";
+  } else {
+    return "finished";
+  }
+};
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("Duaelity");
+    const tournaments = db.collection("tournaments");
+
+    // Fetch all tournaments from the database
+    const allTournaments = await tournaments.find({}).toArray();
+
+    for (const tournament of allTournaments) {
+      // Calculate the state of the tournament
+      const newStatus = calculateTournamentSate(
+        tournament.startDate,
+        tournament.endDate
+      );
+
+      await tournaments.updateOne(
+        {
+          _id: tournament._id,
+        },
+        { $set: { status: newStatus } }
+      );
+    }
+
+    return NextResponse.json({ message: "Cron job executed successfully" });
+  } catch (error) {
+    console.error("Error fetching tournaments:", error);
+    return NextResponse.json(
+      { error: "Failed to execute cron job" },
+      { status: 500 }
+    );
+  }
+}
