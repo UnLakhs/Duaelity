2a8d295a9eae79fe303452b56ce97ab41f938387
UnLakhs <apostolos052@gmail.com>
2025-04-11 16:10:42 +0300

    new version update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Add Cloudinary's domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   }
 };
 
