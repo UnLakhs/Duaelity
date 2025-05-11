839679d41ef548f5e8dfd9caed781ac3ac53b457
UnLakhs <apostolos052@gmail.com>
2025-05-11 03:13:51 +0300

    Updated Nav UI

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
-// import { IoIosSearch } from "react-icons/io";
 import { FaTrophy } from "react-icons/fa";
-import { HiOutlineMenu, HiOutlineX } from "react-icons/hi"; // Burger and close icons
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 import { CiLogin } from "react-icons/ci";
 import Link from "next/link";
 import { User } from "../Cosntants/constants";
 
-// Common styles for nav items
 const navDivStyles = `flex gap-2 cursor-pointer hover:text-white transition-colors duration-300 p-4 justify-start items-center`;
 
 const NavBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to toggle the menu
-  const [user, setUser] = useState<User | null>(null); // State to store user data
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -34,19 +32,18 @@ const NavBar = () => {
     };
     fetchUser();
   }, []);
+
   return (
-    <nav className="bg-[#0f1925] text-[#8392A5] sm:static lg:fixed w-full lg:w-36 lg:h-full z-10 transition-all duration-300">
+    <nav className="bg-[#0f1925] text-[#8392A5] sm:static lg:fixed w-full lg:w-36 lg:h-screen z-10 transition-all duration-300 flex flex-col">
       {/* Logo and burger icon */}
       <div className="flex justify-between items-center p-4">
         <Link href={"/"}>
-          {/* Logo */}
           <img
             src="/images/Duaelity_logo.png"
             alt="Duaelity logo"
             className="object-cover w-12 h-12 lg:w-20 lg:h-20 transition-all duration-300"
           />
         </Link>
-        {/* Burger Menu Button */}
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           className="text-white lg:hidden"
@@ -58,26 +55,38 @@ const NavBar = () => {
       {/* Menu Items */}
       <div
         className={`${
-          isMenuOpen ? "block" : "hidden"
-        } lg:block bg-[#0f1925] lg:bg-transparent w-full lg:h-full transition-all duration-300`}
+          isMenuOpen ? "flex" : "hidden"
+        } flex-col flex-grow lg:flex justify-between bg-[#0f1925] lg:bg-transparent w-full transition-all duration-300`}
       >
-        <Link href={"/tournaments"} className={`${navDivStyles}`}>
-          <FaTrophy size={20} />
-          <span>Tournaments</span>
-        </Link>
-        {user ? (
-          <div className={`${navDivStyles}`}>
-            <span>Welcome {user.username}</span>
-          </div>
-        ) : (
-          <Link
-            href={`/Authentication/SignIn`}
-            className={`${navDivStyles} mt-auto`}
-          >
-            <CiLogin />
-            <span>Log In!</span>
+        <div>
+          <Link href={"/tournaments"} className={`${navDivStyles}`}>
+            <FaTrophy className="w-4 h-5 flex-none" />
+            <span>Tournaments</span>
           </Link>
-        )}
+        </div>
+
+        <div className="lg:mb-4">
+          {user ? (
+            <Link href="/Profile" className={`${navDivStyles}`}>
+              <div className="flex items-center gap-2">
+                <img
+                  src={user.profileImage || "/images/default-avatar.jpg"}
+                  alt="Profile"
+                  className="w-8 h-8 rounded-full object-cover border-2 border-white"
+                />
+                <span className="truncate max-w-[8rem]">{user.username}</span>
+              </div>
+            </Link>
+          ) : (
+            <Link
+              href={`/Authentication/SignIn`}
+              className={`${navDivStyles}`}
+            >
+              <CiLogin />
+              <span>Log In!</span>
+            </Link>
+          )}
+        </div>
       </div>
     </nav>
   );
